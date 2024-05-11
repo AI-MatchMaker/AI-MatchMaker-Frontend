a0bc31fbd5c26f46e1c1d9b6bd3455efda4a9ed0
NevilleQingNY <nevilleqing@gmail.com>
2024-05-11 18:39:07 +0100

    fix: router problem

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router';
+import { createRouter, createWebHashHistory } from 'vue-router';
 import Home from '../views/HomePage.vue';
 // import Contact from './components/Contact.vue';
 import Contact from '../views/ContactPage.vue'
@@ -9,7 +9,7 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes,
 });
 
